00856b995185f9e81fa9b90cee48d118eb9f40f3
Gabriel090696 <gabriel.silva.silas@gmail.com>
2026-04-15 23:41:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import random
+
 from fastapi import FastAPI
 
 app = FastAPI()
 #http://127.0.0.1:8000
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message" : "Hello World"}
+
+
+@app.get("/teste1")
+async def funcaoteste():
+    return {"teste": True,
+            'numero_aleatorio': random.randint(1, 1000)}
